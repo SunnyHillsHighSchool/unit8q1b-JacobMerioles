a2ed70c87d36981420466576df068dbf54881429
Jacob Merioles <>
2021-04-27 18:00:05 +0000

    finished unit 8q1b

---
+++ Unit8_1a.java
@@ -0,0 +1,16 @@
+public class Unit8_1a
+{
+  public int [] [] modifyRows (int [] [] array)
+ { 
+  int[][] newArray = new int[array.length][array[0].length];
+  for (int row = 0; row < newArray.length; row += 2)
+    for (int col = 0; col < newArray[0].length; col++)
+      newArray[row][col] = array[row][col];
+            
+  for (int row = 1; row < newArray.length; row += 2)
+    for (int col = 0; col < newArray[row].length; col++)
+      newArray[row][col] = array[row-1][col];
+  return newArray;
+  }
+}
+ 
